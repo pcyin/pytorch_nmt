f20e0a1f6ae8d541bd656aec4ee88c727ff0e4ec
Pengcheng Yin <pcyin1992@gmail.com>
2017-07-23 20:58:28 -0400

    v1

--- util.py
+++ util.py
@@ -1,5 +1,8 @@
 from collections import defaultdict
 import numpy as np
+import torch
+from torch.autograd import Variable
+
 
 def read_corpus(file_path, source):
     data = []
@@ -13,22 +16,28 @@ def read_corpus(file_path, source):
     return data
 
 
-def batch_slice(data, batch_size, sort=True):
+def batch_slice(data, batch_size):
     batch_num = int(np.ceil(len(data) / float(batch_size)))
     for i in xrange(batch_num):
         cur_batch_size = batch_size if i < batch_num - 1 else len(data) - batch_size * i
-        src_sents = [data[i * batch_size + b][0] for b in range(cur_batch_size)]
-        tgt_sents = [data[i * batch_size + b][1] for b in range(cur_batch_size)]
+        batch_data = [data[i * batch_size + b] for b in xrange(cur_batch_size)]
 
-        if sort:
-            src_ids = sorted(range(cur_batch_size), key=lambda src_id: len(src_sents[src_id]), reverse=True)
-            src_sents = [src_sents[src_id] for src_id in src_ids]
-            tgt_sents = [tgt_sents[src_id] for src_id in src_ids]
-
-        yield src_sents, tgt_sents
+        yield batch_data
 
 
-def data_iter(data, batch_size, shuffle=True):
+def length_array_to_mask_tensor(length_array, cuda=False):
+    max_len = length_array[0]
+    batch_size = len(length_array)
+
+    mask = np.ones((batch_size, max_len), dtype=np.uint8)
+    for i, seq_len in enumerate(length_array):
+        mask[i][:seq_len] = 0
+
+    mask = torch.ByteTensor(mask)
+    return mask.cuda() if cuda else mask
+
+
+def data_iter_ensure_length(data, batch_size, shuffle=True):
     """
     randomly permute data, then sort by source length, and partition into batches
     ensure that the length of source sentences in each batch is decreasing
@@ -43,9 +52,81 @@ def data_iter(data, batch_size, shuffle=True):
     for src_len in buckets:
         tuples = buckets[src_len]
         if shuffle: np.random.shuffle(tuples)
-        batched_data.extend(list(batch_slice(tuples, batch_size)))
+
+        batches = batch_size(tuples, batch_size)
+        for batch in batches:
+            src_sents, tgt_sents = zip(*batch)
+            cur_batch_size = len(batch)
+
+            src_ids = sorted(range(cur_batch_size), key=lambda src_id: len(src_sents[src_id]), reverse=True)
+            src_sents = [src_sents[src_id] for src_id in src_ids]
+            tgt_sents = [tgt_sents[src_id] for src_id in src_ids]
+
+            batched_data.append((src_sents, tgt_sents))
 
     if shuffle:
         np.random.shuffle(batched_data)
     for batch in batched_data:
         yield batch
+
+
+def data_iter(data, batch_size, shuffle=True):
+    """
+        randomly permute data and partition into batches
+    """
+    data_size = len(data)
+    indices = np.array(range(data_size))
+
+    if shuffle:
+        np.random.shuffle(indices)
+
+    batch_slices = batch_slice(indices, batch_size)
+    for item_indices in batch_slices:
+        batch = [data[i] for i in item_indices]
+        src_sents, tgt_sents = zip(*batch)
+
+        src_ids = sorted(range(len(batch)), key=lambda src_id: len(src_sents[src_id]), reverse=True)
+        src_sents = [src_sents[src_id] for src_id in src_ids]
+        tgt_sents = [tgt_sents[src_id] for src_id in src_ids]
+
+        yield src_sents, tgt_sents
+
+
+def to_input_variable(sents, vocab, cuda=False, is_test=False):
+    """
+    return a tensor of shape (src_sent_len, batch_size)
+    """
+
+    word_ids = word2id(sents, vocab)
+    sents_t, masks = input_transpose(word_ids, vocab['<pad>'])
+
+    sents_var = Variable(torch.LongTensor(sents_t), volatile=is_test, requires_grad=False)
+    if cuda:
+        sents_var = sents_var.cuda()
+
+    return sents_var
+
+
+def input_transpose(sents, pad_token):
+    max_len = max(len(s) for s in sents)
+    batch_size = len(sents)
+
+    sents_t = []
+    masks = []
+    for i in xrange(max_len):
+        sents_t.append([sents[k][i] if len(sents[k]) > i else pad_token for k in xrange(batch_size)])
+        masks.append([1 if len(sents[k]) > i else 0 for k in xrange(batch_size)])
+
+    return sents_t, masks
+
+
+def word2id(sents, vocab):
+    if type(sents[0]) == list:
+        return [[vocab[w] for w in s] for s in sents]
+    else:
+        return [vocab[w] for w in sents]
+
+
+def tensor_transform(linear, X):
+    # X is a 3D tensor
+    return linear(X.contiguous().view(-1, X.size(2))).view(X.size(0), X.size(1), -1)
