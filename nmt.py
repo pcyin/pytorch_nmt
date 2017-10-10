76bc58457971fd337d540b5a00b2f3378872b24a
Pengcheng Yin <pcyin1992@gmail.com>
2017-10-09 22:43:28 -0400

    added RAML

--- nmt.py
+++ nmt.py
@@ -29,7 +29,7 @@ def init_config():
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', default=5783287, type=int, help='random seed')
     parser.add_argument('--cuda', action='store_true', default=False, help='use gpu')
-    parser.add_argument('--mode', choices=['train', 'raml_train', 'test', 'sample', 'prob', 'interactive'],
+    parser.add_argument('--mode', choices=['train', 'raml_train', 'sqdml_train', 'test', 'sample', 'prob', 'interactive'],
                         default='train', help='run mode')
     parser.add_argument('--vocab', type=str, help='path of the serialized vocabulary')
     parser.add_argument('--batch_size', default=32, type=int, help='batch size')
@@ -488,7 +488,7 @@ def read_raml_train_data(data_file, temp):
     return train_data
 
 
-def train_raml(args):
+def train_sqdml(args):
     tau = args.temp
 
     dataset = Dataset(args.data_folder)
@@ -513,7 +513,7 @@ def train_raml(args):
     cum_examples = cum_batches = report_examples = epoch = valid_num = best_model_iter = 0
     hist_valid_scores = []
     train_time = begin_time = time.time()
-    print('begin RAML training')
+    print('begin SQDML training', file=sys.stderr)
 
     # smoothing function for BLEU
     sm_func = None
@@ -609,6 +609,222 @@ def train_raml(args):
             src_images_var = src_images_var.unsqueeze(1)\
                 .expand(src_images_var.size(0), args.sample_size, src_images_var.size(1))\
                 .contiguous().view(-1, src_images_var.size(1))
+            tgt_sents_var = sent_to_input_variable(raml_tgt_sents, vocab, cuda=args.cuda)
+            weights_var = Variable(torch.FloatTensor(raml_tgt_weights), requires_grad=False)
+            if args.cuda:
+                weights_var = weights_var.cuda()
+
+            batch_size = len(raml_src_images)  # batch_size = args.batch_size * args.sample_size
+            pred_tgt_word_num = sum(len(s[1:]) for s in raml_tgt_sents)  # omitting leading `<s>`
+            optimizer.zero_grad()
+
+            # (tgt_sent_len, batch_size, tgt_vocab_size)
+            scores = model(src_images_var, tgt_sents_var[:-1])
+            # (tgt_sent_len * batch_size, tgt_vocab_size)
+            log_scores = F.log_softmax(scores.view(-1, scores.size(2)))
+            # remove leading <s> in tgt sent, which is not used as the target
+            flattened_tgt_sents = tgt_sents_var[1:].view(-1)
+
+            # batch_size * tgt_sent_len
+            tgt_log_scores = torch.gather(log_scores, dim=1, index=flattened_tgt_sents.unsqueeze(1)).squeeze(1)
+            unweighted_loss = -tgt_log_scores * (1. - torch.eq(flattened_tgt_sents, 0).float())  # apply mask
+            weighted_loss = unweighted_loss * weights_var.repeat(scores.size(0))
+            weighted_loss = weighted_loss.sum()
+            weighted_loss_val = weighted_loss.data[0]
+            nll_loss_val = unweighted_loss.sum().data[0]
+            # weighted_log_scores = log_scores * weights.view(-1, scores.size(2))
+            # weighted_loss = nll_loss(weighted_log_scores, flattened_tgt_sents)
+
+            loss = weighted_loss / batch_size
+            # nll_loss_val = nll_loss(log_scores, flattened_tgt_sents).data[0]
+
+            loss.backward()
+            # clip gradient
+            grad_norm = torch.nn.utils.clip_grad_norm(model.parameters(), args.clip_grad)
+            optimizer.step()
+
+            report_weighted_loss += weighted_loss_val
+            cum_weighted_loss += weighted_loss_val
+            report_loss += nll_loss_val
+            cum_loss += nll_loss_val
+            report_tgt_words += pred_tgt_word_num
+            cum_tgt_words += pred_tgt_word_num
+            report_examples += batch_size
+            cum_examples += batch_size
+            cum_batches += batch_size
+
+            if train_iter % args.log_every == 0:
+                print('epoch %d, iter %d, avg. loss %.2f, '
+                      'avg. ppl %.2f cum. examples %d, '
+                      'speed %.2f words/sec, time elapsed %.2f sec' % (epoch, train_iter,
+                                                                       report_weighted_loss / report_examples,
+                                                                       np.exp(report_loss / report_tgt_words),
+                                                                       cum_examples,
+                                                                       report_tgt_words / (time.time() - train_time),
+                                                                       time.time() - begin_time),
+                      file=sys.stderr)
+
+                train_time = time.time()
+                report_loss = report_weighted_loss = report_tgt_words = report_examples = 0.
+
+            # perform validation
+            if train_iter % args.valid_niter == 0:
+                print('epoch %d, iter %d, cum. loss %.2f, '
+                      'cum. ppl %.2f cum. examples %d' % (epoch, train_iter,
+                                                          cum_weighted_loss / cum_batches,
+                                                          np.exp(cum_loss / cum_tgt_words),
+                                                          cum_examples),
+                      file=sys.stderr)
+
+                cum_loss = cum_weighted_loss = cum_batches = cum_tgt_words = 0.
+                valid_num += 1
+
+                print('begin validation ...', file=sys.stderr)
+                model.eval()
+
+                # compute dev. ppl and bleu
+                dev_hyps = decode(model, dataset.valid_examples())
+                dev_hyps = [hyps[0] for hyps in dev_hyps]
+                if args.valid_metric == 'bleu':
+                    valid_metric = get_bleu([tgt for id, src, tgt in dataset.valid_examples()], dev_hyps)
+                else:
+                    valid_metric = get_acc([tgt for id, src, tgt in dataset.valid_examples()], dev_hyps, acc_type=args.valid_metric)
+                print('validation: iter %d, dev. %s %f' % (train_iter, args.valid_metric, valid_metric),
+                      file=sys.stderr)
+
+                model.train()
+
+                is_better = len(hist_valid_scores) == 0 or valid_metric > max(hist_valid_scores)
+                is_better_than_last = len(hist_valid_scores) == 0 or valid_metric > hist_valid_scores[-1]
+                hist_valid_scores.append(valid_metric)
+
+                if valid_num > args.save_model_after:
+                    model_file = args.save_to + '.iter%d.bin' % train_iter
+                    print('save model to [%s]' % model_file, file=sys.stderr)
+                    model.save(model_file)
+
+                if (not is_better_than_last) and args.lr_decay:
+                    lr = optimizer.param_groups[0]['lr'] * args.lr_decay
+                    print('decay learning rate to %f' % lr, file=sys.stderr)
+                    optimizer.param_groups[0]['lr'] = lr
+
+                if is_better:
+                    patience = 0
+                    best_model_iter = train_iter
+
+                    if valid_num > args.save_model_after:
+                        print('save currently the best model ..', file=sys.stderr)
+                        model_file_abs_path = os.path.abspath(model_file)
+                        symlin_file_abs_path = os.path.abspath(args.save_to + '.bin')
+                        os.system('ln -sf %s %s' % (model_file_abs_path, symlin_file_abs_path))
+                else:
+                    patience += 1
+                    print('hit patience %d' % patience, file=sys.stderr)
+                    if patience == args.patience:
+                        print('early stop!', file=sys.stderr)
+                        print('the best model is from iteration [%d]' % best_model_iter, file=sys.stderr)
+                        exit(0)
+
+
+def train_raml(args):
+    tau = args.temp
+
+    dataset = Dataset(args.data_folder)
+
+    vocab, model, optimizer, nll_loss, cross_entropy_loss = init_training(args)
+    print('num. target words in vocab: %d' % len(vocab), file=sys.stderr)
+
+    if args.raml_sample_mode == 'pre_sample':
+        # dict of (src, [tgt: (sent, prob)])
+        print('read in raml training data...', file=sys.stderr, end='')
+        begin_time = time.time()
+        raml_samples = read_raml_train_data(args.raml_sample_file, temp=tau)
+        print('done[%d s].' % (time.time() - begin_time))
+    elif args.raml_sample_mode.startswith('hamming_distance'):
+        print('sample from hamming distance payoff distribution', file=sys.stderr)
+        payoff_prob, Z_qs = generate_hamming_distance_payoff_distribution(max_sent_len=args.decode_max_time_step,
+                                                                          vocab_size=len(vocab) - 3,
+                                                                          tau=tau)
+
+    train_iter = patience = cum_loss = report_loss = cum_tgt_words = report_tgt_words = 0
+    report_weighted_loss = cum_weighted_loss = 0
+    cum_examples = cum_batches = report_examples = epoch = valid_num = best_model_iter = 0
+    hist_valid_scores = []
+    train_time = begin_time = time.time()
+    print('begin RAML training', file=sys.stderr)
+
+    # smoothing function for BLEU
+    sm_func = None
+    if args.smooth_bleu:
+        sm_func = SmoothingFunction().method3
+
+    while True:
+        epoch += 1
+        for batch_examples in dataset.batch_iter('train', batch_size=args.batch_size, shuffle=True):
+            train_iter += 1
+
+            raml_src_images = []
+            raml_tgt_sents = []
+            raml_tgt_weights = []
+
+            if args.raml_sample_mode == 'pre_sample':
+                raise NotImplementedError()
+            elif args.raml_sample_mode in ['hamming_distance', 'hamming_distance_impt_sample']:
+                for image_id, src_image, tgt_sents in batch_examples:
+                    # sample args.sample_size sentences from each target
+                    for tgt_sent in tgt_sents:
+                        tgt_samples = []  # make sure the ground truth y* is in the samples
+                        tgt_sent_len = len(tgt_sent) - 2  # remove <s> and </s> # TODO: check if ends with period!
+                        tgt_ref_tokens = tgt_sent[1:-1]
+                        bleu_scores = []
+                        # print('y*: %s' % ' '.join(tgt_sent))
+                        # sample an edit distances
+                        e_samples = np.random.choice(range(tgt_sent_len + 1), p=payoff_prob[tgt_sent_len],
+                                                     size=args.sample_size, replace=True)
+
+                        # make sure the ground truth y* is in the samples
+                        if args.raml_bias_groundtruth and (not 0 in e_samples):
+                            e_samples[0] = 0
+
+                        for i, e in enumerate(e_samples):
+                            if e > 0:
+                                # sample a new tgt_sent $y$
+                                old_word_pos = np.random.choice(range(1, tgt_sent_len + 1), size=e, replace=False)
+                                new_words = [vocab.id2word[wid] for wid in
+                                             np.random.randint(3, len(vocab), size=e)]
+                                new_tgt_sent = list(tgt_sent)
+                                for pos, word in zip(old_word_pos, new_words):
+                                    new_tgt_sent[pos] = word
+                            else:
+                                new_tgt_sent = list(tgt_sent)
+
+                            # if enable importance sampling, compute bleu score
+                            if args.raml_sample_mode == 'hamming_distance_impt_sample':
+                                if e > 0:
+                                    # remove <s> and </s>
+                                    bleu_score = sentence_bleu([tgt_ref_tokens], new_tgt_sent[1:-1],
+                                                               smoothing_function=sm_func)
+                                    bleu_scores.append(bleu_score)
+                                else:
+                                    bleu_scores.append(1.)
+
+                            # print('y: %s' % ' '.join(new_tgt_sent))
+                            tgt_samples.append(new_tgt_sent)
+
+                        # if enable importance sampling, compute importance weight
+                        if args.raml_sample_mode == 'hamming_distance_impt_sample':
+                            tgt_sample_weights = [math.exp(bleu_score / tau) / math.exp(-e / tau) for e, bleu_score in
+                                                  zip(e_samples, bleu_scores)]
+                            normalizer = sum(tgt_sample_weights)
+                            tgt_sample_weights = [w / normalizer for w in tgt_sample_weights]
+                        else:
+                            tgt_sample_weights = [1.] * args.sample_size
+
+                        raml_src_images.extend([src_image] * len(tgt_samples))
+                        raml_tgt_sents.extend(tgt_samples)
+                        raml_tgt_weights.extend(tgt_sample_weights)
+
+            src_images_var = Variable(torch.stack(raml_src_images), requires_grad=False)
             tgt_sents_var = sent_to_input_variable(raml_tgt_sents, vocab, cuda=args.cuda)
             weights_var = Variable(torch.FloatTensor(raml_tgt_weights), requires_grad=False)
             if args.cuda:
@@ -956,6 +1172,8 @@ if __name__ == '__main__':
 
     if args.mode == 'train':
         train(args)
+    elif args.mode == 'sqdml_train':
+        train_sqdml(args)
     elif args.mode == 'raml_train':
         train_raml(args)
     elif args.mode == 'sample':
